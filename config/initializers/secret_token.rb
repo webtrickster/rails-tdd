69731745f608e5653aea88ab3c653e4892570ab4
deepetch <gopi@Gopis-Mac-mini.local>
2013-11-01 01:21:41 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'f973053fbf7615270864e3222015d39f0ce6395eca2433f46187d884600ba1b9a76ba61a1540b46750eccfb55c592e6b3a44177cefbbef553eb28fc0270c7bd5'
